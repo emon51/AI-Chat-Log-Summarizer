1a6e8f5d52d2b962233fe3e86c90846786affa31
EMON51 <emanur11235@gmail.com>
2025-06-27 20:20:08 +0600

    Final commit

--- chat_summarizer.py
+++ chat_summarizer.py
@@ -1,3 +1,4 @@
+
 from nltk.corpus import stopwords 
 import heapq 
 
@@ -10,6 +11,9 @@ class AI_CLS:
         self.ai_messages = []
         self.non_stop_words_map = {} #Store data as key a value pair (word, frequency)
         self.non_stop_words_max_heap = [] #Store data as a pair -> (frequency, word)   
+
+        self.chat_log_parsing()
+        self._get_map_and_heap()
 
     def chat_log_parsing(self):
         user_messages, ai_messages = [], []
@@ -27,31 +31,25 @@ class AI_CLS:
     
     def count_messages(self):
         user_msg_count, ai_msg_count = len(self.user_messages), len(self.ai_messages)
-        total_msg = user_msg_count + ai_msg_count
-        print(f'Total number of conversation: {total_msg}')
-        print(f"User's number of messages: {user_msg_count}")
-        print(f"AI's number of messages: {user_msg_count}")
-        return 
+        total_msg_count = user_msg_count + ai_msg_count
+        return [user_msg_count, ai_msg_count, total_msg_count]
 
 
     def _get_map_and_heap(self):
-        all_messages = self.user_messages + self.ai_messages
+        all_messages = self.user_messages + self.ai_messages #Concatenate two list of messages.
         for line in all_messages:
             line = line.lower()
             for word in line.split():
                 new_word = [ch for ch in word if ch.isalpha()]
                 new_word = ''.join(new_word)
-                #print(new_word)
                 if new_word not in self.stop_words: 
                     self.non_stop_words_map[new_word] = self.non_stop_words_map.get(new_word, 0) + 1
                     
-        #print(self.non_stop_words_map)
         for word, freq in self.non_stop_words_map.items():
             heapq.heappush(self.non_stop_words_max_heap, [-freq, word])
         
     
     def get_most_used_words(self):
-        self._get_map_and_heap()
         five_most_used_words = []
         count = 5
         temp_arr = []
@@ -61,7 +59,7 @@ class AI_CLS:
             five_most_used_words.append(word)
             count -= 1
         
-        #push back all popped elements again into heap.
+        #push back all popped elements again into the heap.
         while temp_arr:
             freq, word = temp_arr.pop()
             heapq.heappush(self.non_stop_words_max_heap, [freq, word])
@@ -70,30 +68,24 @@ class AI_CLS:
     
     
     def get_summary(self):
-        total_message_exchange = len(self.user_messages) + len(self.ai_messages)
-        nature_keyword = self.non_stop_words_max_heap[0][1]
+        user_msg_count, ai_msg_count, total_msg_count = self.count_messages()
+        total_message_exchange = total_msg_count
+        nature_keyword = self.non_stop_words_max_heap[0][1].capitalize()
         most_common_keywords = self.get_most_used_words()
 
         print('Summary: ')
-        print(f"- The conversation had {total_message_exchange} exchanges.")
+        print(f"- The conversation had {total_message_exchange} exchanges; User: {user_msg_count} and AI: {ai_msg_count}.")
         print(f"- The user mainly asked about {nature_keyword}.")
-        print(f"- The most common keywords: {most_common_keywords}.")
+        print(f"- The most common used keywords: {', '.join(most_common_keywords)}.")
     
                     
 
 
 
-
-                    
-
-    
 
 
 if __name__ == '__main__':
     chat_file = 'chat.txt' 
-    aichat = AI_CLS(chat_file)
-    aichat.chat_log_parsing()
-    #aichat.count_messages()
-    aichat.get_most_used_words()
-    aichat.get_summary()
+    chat = AI_CLS(chat_file)
+    chat.get_summary()
   
