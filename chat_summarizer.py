27c3d86e4821d344994c443bdf00829b5aa3874e
EMON51 <emanur11235@gmail.com>
2025-06-27 12:23:57 +0600

    Implemented count_messages method

--- chat_summarizer.py
+++ chat_summarizer.py
@@ -5,25 +5,36 @@ class AI_CLS:
     def __init__(self, chat_log):
         self.stop_words = set(stopwords.words('english'))
         self.chat_log = chat_log
+        self.user_messages = []
+        self.ai_messages = []
     
-    def parse_chatting(self):
+    def chat_log_parsing(self):
         user_messages, ai_messages = [], []
         try:
             with open(self.chat_log, 'r') as file:
                 for line in file:
                     line = line.strip()
                     if line.startswith('User:'):
-                        user_messages.append(line[5:].strip())
+                        self.user_messages.append(line[5:].strip())
                     elif line.startswith('AI:'):
-                        ai_messages.append(line[3:].strip())
-            return [user_messages, ai_messages]
+                        self.ai_messages.append(line[3:].strip())
         except Exception as e:
             print(f"An error occurred while reading the file: {e}")
             return 
+    
+    def count_messages(self):
+        user_msg_count, ai_msg_count = len(self.user_messages), len(self.ai_messages)
+        total_msg = user_msg_count + ai_msg_count
+        print(f'Total number of conversation: {total_msg}')
+        print(f"User's number of messages: {user_msg_count}")
+        print(f"AI's number of messages: {user_msg_count}")
+        return 
+    
     
 
 if __name__ == '__main__':
     chat_file = 'chat.txt' 
     aichat = AI_CLS(chat_file)
-    res = aichat.parse_chatting()
-    print(res)
+    aichat.chat_log_parsing()
+    aichat.count_messages()
+  
